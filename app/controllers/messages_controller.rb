f44237d7f3ac3c0c4db55a94ac3c774111115bf1
Tadesse Alemayehu <tadalem26@gmail.com>
2022-12-21 16:57:51 +0300

    Use turbo stream to submit message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,9 +25,12 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
+        format.turbo_stream  {render turbo_stream: turbo_stream.update('new_message', partial: "form", locals:{message: Message.new})}
+
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
+        format.turbo_stream  {render turbo_stream: turbo_stream.update('new_message', partial: "form", locals:{message: @message})}
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
